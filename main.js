36466eaa00931c87786b84de43839c8f28cdea44
ccavad <88976928+ccavad@users.noreply.github.com>
2022-03-24 01:36:58 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+
+  $(window).on('load', parolYarat); // sehife yuklenen kimi parol yaradacaq
+
+  // variables
+
+  const uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  const lowercase = "qwertyiopasdfghjklzxcvbnm";
+  const numbers = "1234567890";
+  const special = "~`!@#$%^&*()-_+={}[]|\\;:\"<>,./?";
+  const uzunluq = $("#uzunluq"); // parolun uzunlugunu idare eden range inputu
+  let generatedParol;
+  const uzunluqspan = $("#uzunluqspan"); // parolun uzunlugunu gosteren label
+  const parol = $("#parol"); // parolun goruneceyi text inputu
+
+
+  // buttons
+
+  uzunluq.change(()=>{
+    uzunluqspan.html(uzunluq.val());
+    parolYarat()
+    if(uzunluq.val() < 7){
+      parol.css({"border": "2px solid red", "color": "red"})
+    } else if (uzunluq.val() >8) {
+      parol.css({"border": "2px solid green", "color": "green"})
+    } else {
+      parol.css({"border": "2px solid yellow", "color": "yellow"})
+    }
+  })
+
+  $("#generateBtn").click(()=>{
+    parolYarat()
+  })
+
+  $("#copyBtn").click(()=>{
+    parol.select();
+    document.execCommand("Copy");
+    if(parol.val() != 0){ // parolun kopyalandigini bildiren popup cixardacaq
+      $(".notif").show(250)
+      setTimeout(() => {
+      $(".notif").hide(250)
+      }, 3000);
+    }
+  })
+
+  $(".checkboxcont input").click(()=>{parolYarat()})
+
+  // funksiyalar
+
+  function parolYarat(){
+    generatedParol = "";
+    generateRndm();
+    parol.val(generatedParol);
+  }
+
+  function generateRndm(){
+
+    let allChar = "";
+    ($("#check1").is(":checked") === true) ? allChar += uppercase : {};
+    ($("#check2").is(":checked") === true) ? allChar += lowercase : {};
+    ($("#check3").is(":checked") === true) ? allChar += numbers : {};
+    ($("#check4").is(":checked") === true) ? allChar += special : {};
+    allChar = allChar.split("")
+
+    if (allChar.length == 0) {
+      parol.attr("placeholder", "parol seçiminiz boşdur")
+    } else {
+      for(i=0; i<uzunluq.val(); i++){
+        generatedParol += allChar[Math.floor(Math.random() * allChar.length)]
+      }
+      return generatedParol;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
